4e8243a744e0dce0237a40695328c231266c014b
Yujin Oh <yujin2e@ewhain.net>
2022-06-20 18:39:26 +0900

    cart count effect

--- ewha-react-publishing/src/pages/HomePage.tsx
+++ ewha-react-publishing/src/pages/HomePage.tsx
@@ -80,7 +80,7 @@ export const HomePage = () => {
     setSearch(e.target.value);
   };
 
-
+  // cart 수량 변경용
   function onClick() {
     setCount(count + 1);
   }
@@ -88,6 +88,8 @@ export const HomePage = () => {
   function onClick2() {
     setCount(count - 1);
   }
+
+
   // strapi에 저장된 products api 가져오기 
   const getProductData = async () => {
     const url = `https://startup-coding-hbd.herokuapp.com/api/products`
@@ -247,7 +249,7 @@ export const HomePage = () => {
                   <svg className="h-5 w-5" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                 </button>
                 <span className="text-gray-700 mx-2">{count}</span>
-                <button className="text-gray-500 focus:outline-none focus:text-gray-600">
+                <button onClick={onClick2} className="text-gray-500 focus:outline-none focus:text-gray-600">
                   <svg className="h-5 w-5" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                 </button>
               </div>
