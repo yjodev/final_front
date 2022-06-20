30744735cf3b2673642d89eb18e9157b6323d38c
Yujin Oh <yujin2e@ewhain.net>
2022-06-20 13:44:00 +0900

    initial page to loginpage

--- ewha-react-publishing/src/pages/Router.tsx
+++ ewha-react-publishing/src/pages/Router.tsx
@@ -14,6 +14,8 @@ export const Router = () => {
       <Route path="/signup" component={SignupPage} />
       <Route path="/login" component={LoginPage} />
       {/* <Route path="/" component={HomePage} exact /> */}
+      <Route path="/" component={LoginPage} exact />
+
       <Route path="/follow" component={FollowPage} />
       <Route path="/rooms/:id" component={MessagePage} exact />
 
