e9d062454cd6a7c049e959901877ebb1d1e0e7c9
yjodev <101042643+yjodev@users.noreply.github.com>
2022-06-26 03:37:25 +0900

    axios

--- ewha-react-publishing/src/pages/NewProductPage.tsx
+++ ewha-react-publishing/src/pages/NewProductPage.tsx
@@ -52,35 +52,43 @@ export const NewProductPage = () => {
 
 	return <div>
     <div className="container mx-auto px-6 py-3">
-        <div className="flex items-center justify-between space-y-">
-            
-            <div className="hidden w-full text-gray-600 md:flex md:items-center">
-                
-            </div>
-       <button className="w-full text-gray-700 md:text-center text-2xl font-semibold">
-            상품 추가하기
-        </button>
-     
-        <div className="flex items-center justify-end w-full">
-            <Link to="/homepage">
-                <button className="text-gray-600 focus:outline-none mx-4 sm:mx-0" >
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 stroke-slate-400 mr-4 group-hover:stroke-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
-                    </svg>
-                </button>
+
+                <div className="flex items-center justify-between">
+
+          <div className="hidden w-full text-gray-600 md:flex md:items-center space-x-3">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 stroke-slate-400 mr-4 group-hover:stroke-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
+
+            </svg>
+            <Link to="/follow">
+              <button className="mx-1 text-sm">Follower</button>
             </Link>
+          </div>
+
+          <button className="w-full text-gray-700 md:text-center text-2xl font-semibold">
+            상품 추가하기
+          </button>
+
+          <div className="flex items-center justify-end w-full">
+                        <Link to="/homepage">
+                            <button className="text-gray-600 focus:outline-none mx-4 sm:mx-0" onClick={() => setOpen(true)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 stroke-slate-400 mr-4 group-hover:stroke-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
+                                </svg>
+                            </button>
+                        </Link>
 
 
-            <div className="flex sm:hidden">
-                <button type="button" className="text-gray-600 hover:text-gray-500 focus:outline-none focus:text-gray-500" aria-label="toggle menu">
-                    <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
-                        <path fillRule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
-                    </svg>
-                </button>
-            </div>
+                        <div className="flex sm:hidden">
+                            <button type="button" className="text-gray-600 hover:text-gray-500 focus:outline-none focus:text-gray-500" aria-label="toggle menu">
+                                <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
+                                    <path fillRule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+
         </div>
-
-    </div>
 
 </div>
 
@@ -137,3 +145,7 @@ export const NewProductPage = () => {
     
 
 }
+function setOpen(arg0: boolean): void {
+    throw new Error('Function not implemented.');
+}
+
